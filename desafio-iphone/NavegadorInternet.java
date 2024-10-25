2fc664b8b02884cf7a011632d1e6e6ed7d1f1787
LinkMoura <160137873+LinkMoura@users.noreply.github.com>
2024-10-25 10:48:53 -0300

    Delete desafio-iphone/NavegadorInternet.java

--- desafio-iphone/NavegadorInternet.java
+++
@@ -1,7 +0,0 @@
-package sistema.iphone.navegador;
-
-public interface NavegadorInternet {
-    public void exibirPagina(String url);
-    public void adicionarNovaAba();
-    public void atualizarPagina();
-}
